740351c6fb672b8133cc1165b9badaa36f27de27
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-02-18 03:32:45 -0500

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const port = 5000
 
 const { produce } = require("./data.js")
+
+app.use(cors())
 
 app.get("/api/produce", (req, res) => {
   res.json(produce)
